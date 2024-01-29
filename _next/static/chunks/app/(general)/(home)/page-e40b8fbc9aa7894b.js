f9d640dce4db92e66a0a9d3253b12386dce3df94
Your Name <you@example.com>
2024-01-29 12:06:44 +0700

    first commit

---
+++ _next/static/chunks/app/(general)/(home)/page-e40b8fbc9aa7894b.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[228],{480567:function(e,n,s){Promise.resolve().then(s.t.bind(s,990413,23)),Promise.resolve().then(s.t.bind(s,68326,23))}},function(e){e.O(0,[413,326,971,472,744],function(){return e(e.s=480567)}),_N_E=e.O()}]);
